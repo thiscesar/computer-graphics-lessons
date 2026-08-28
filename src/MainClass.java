8514464bffc2f78b5594366f6ce7796373b7f042
thiscesar <cesarldsousa@gmail.com>
2026-08-28 18:34:56 -0300

    melhorias legibilidade codigo

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,13 +6,13 @@ import javax.swing.JFrame;
 public class MainClass {
 	public static void main(String[] args) {
 		MainCanvas meuCanvas = new MainCanvas();
-		
+
 		JFrame f = new JFrame();
-		f.setSize(640, 480);
+        // talvez separar e não chumbar as dimensões do Canvsa com a janela.
+		f.setSize(MainCanvas.WIDTH, MainCanvas.HEIGHT);
 		f.setVisible(true);
 		f.getContentPane().add(meuCanvas);
 		
-	
 		f.addWindowListener(new WindowAdapter() {
 		    @Override
 		    public void windowClosing(WindowEvent e) {
